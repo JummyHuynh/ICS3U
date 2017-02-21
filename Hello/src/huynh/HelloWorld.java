9edffc6c245c063e911320ce5aa027c9f931b02b
Jimmy Huynh <323819508@C-5LQJHS1.tdsb.on.ca>
2017-02-21 14:52:21 -0500

    Adding a new print line

--- Hello/src/huynh/HelloWorld.java
+++ Hello/src/huynh/HelloWorld.java
@@ -2,7 +2,7 @@ package huynh;
 
 /**
  * 
- * Hello World
+ * Hello
  * Prints "Hello World"
  * FEB.21.2017
  * @author Jimmy Huynh
@@ -14,7 +14,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-	
+		System.out.println("My first program");
+
 	}
 
 }
