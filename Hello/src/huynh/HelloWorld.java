c14b2f3fd5a6468d055f22451af965f84642c804
Jimmy Huynh <323819508@C-5LQJHS1.tdsb.on.ca>
2017-02-21 14:45:19 -0500

    Pushing the Hello World program

---
+++ Hello/src/huynh/HelloWorld.java
@@ -0,0 +1,20 @@
+package huynh;
+
+/**
+ * 
+ * Hello World
+ * Prints "Hello World"
+ * FEB.21.2017
+ * @author Jimmy Huynh
+ * 
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	
+	}
+
+}
