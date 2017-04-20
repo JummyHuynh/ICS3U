d9c410ea341b55064a7c73d6ad2956ff689f6e39
jimmy <jimmy@192.168.0.21>
2017-04-20 03:28:13 -0400

    Added the change of capitals for the reversed sentence file, pushing pythagorean file though I am not yet finished

--- Unit2/src/huynh/ReversedSentence.java
+++ Unit2/src/huynh/ReversedSentence.java
@@ -6,25 +6,30 @@ public class ReversedSentence {
 
 	public static void main(String[] args) {
 
-		// Declarations
+		// Declaration
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Enter in the sentence that will be reversed");
 		String [] sentence = scan.nextLine().split(" ");
-		
+		String[] newSentence = sentence[0].split("");
+
 		for (int i = 0; i < sentence.length; i++) {
 			for (int j = sentence[i].length()-1; j>=0; j--) {
-				System.out.print(sentence[i].charAt(j));
-				
-				
-				
-				//String theSentence = changeCase.toUpperCase(sentence[i].charAt(j));
+
+
+				// Replaces first capital when needed
+				if ((i == 0) && (j == sentence[i].length()-1) && (newSentence[0].equals(newSentence[0].toUpperCase()))) {
+					System.out.print(newSentence[sentence[0].length()-1].toUpperCase());	
+				}
+				else if ((i == 0) && (j == 0) && (newSentence[sentence[0].length()-1]).equals(newSentence[sentence[0].length()-1].toLowerCase())) {
+					System.out.print(newSentence[0].toLowerCase());
+				}
+				else
+					System.out.print(sentence[i].charAt(j));
+
 			}
 			System.out.print(" ");
-			
-		}
-		
 
+		}	
 	}
-
 }
